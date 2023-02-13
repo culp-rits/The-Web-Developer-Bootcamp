5e184e67b12536e3ae6b48d0d94857f453e642ae
Ritheesh Kumar <114294870+culp-rits@users.noreply.github.com>
2023-02-13 20:20:33 +0530

    Create index.js

---
+++ Coding Exercise/24/index.js
@@ -0,0 +1,5 @@
+const mystery = 'Pflsdkfsl7kfj'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+
+if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
+    console.log("YOU GOT IT!!!");
+}

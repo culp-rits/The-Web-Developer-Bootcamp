54c6acf7e96e77620085e990a6093557f4c20019
Ritheesh Kumar <114294870+culp-rits@users.noreply.github.com>
2023-03-01 23:55:10 +0530

    Create app.js

---
+++ Coding Exercise/56/app.js
@@ -0,0 +1,7 @@
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+let spans = document.querySelectorAll('span');
+for(let i in colors){
+    spans[i].style.color = colors[i];
+    console.log(i)
+}

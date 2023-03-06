d04b41274c4a720edba58f2661ee0b1dbde88754
Ritheesh Kumar <114294870+culp-rits@users.noreply.github.com>
2023-03-06 06:13:19 +0530

    Create app.js

---
+++ Coding Exercise/58/app.js
@@ -0,0 +1,6 @@
+for(let i=0;i<100;i++){
+    let but = document.createElement('button');
+    but.innerText = 'Hey!';
+    let container = document.querySelector('#container');
+    container.appendChild(but);
+}
